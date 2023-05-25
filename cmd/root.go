367ba26bb106527c3d17001e719a448ee46f1276
Akash Chouhan <akash.c1500@gmail.com>
2023-05-25 17:51:39 +0530

    :test_tube: cmd fixes and cosmetic updates

--- cmd/root.go
+++ cmd/root.go
@@ -1,34 +1,39 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
-*/
+/* ***********************************************
+ * copyright: 2023 @github.com/akashchouhan16/traceip
+ * author: @akashchouhan16
+ *************************************************/
 package cmd
 
 import (
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "traceip",
-	Short: "traceip is a command line toolkit for ip tracing.",
-	Long: `Usage: traceip [-h] [--help] [-v] [--version] [-d] [-h maximum_hops] [-j host-list] [-w timeout] 
-               [-R] [-S srcaddr] [-4] [-6] target_name
-
-Options:
-    -d                 Do not resolve addresses to hostnames.
-    -h maximum_hops    Maximum number of hops to search for target.
-    -j host-list       Loose source route along host-list (IPv4-only).
-    -w timeout         Wait timeout milliseconds for each reply.
-    -R                 Trace round-trip path (IPv6-only).
-    -S srcaddr         Source address to use (IPv6-only).
-    -4                 Force using IPv4.
-    -6                 Force using IPv6.`,
+	Short: color.HiGreenString("traceip is a command line tool for geolocation lookups on IP addresses. Visit https://github.com/akashchouhan16/traceip for more details."),
+	Long: color.HiGreenString(`
+ ======================================================	
+| ████████╗██████╗  █████╗  ██████╗███████╗ ██╗██████╗  |
+| ╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██╔════╝ ██║██╔══██╗ |
+|    ██║   ██████╔╝███████║██║     █████╗   ██║██████╔╝ |
+|    ██║   ██╔══██╗██╔══██║██║     ██╔══╝   ██║██╔═══╝  |
+|    ██║   ██║  ██║██║  ██║╚██████╗███████╗ ██║██║      |
+|    ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝ ╚═╝╚═╝      |
+ ====================================================== 
+A command line tool for geolocation lookups on IP addresses.
+ `),
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	// Run: func(cmd *cobra.Command, args []string) {
+	// 	if len(args) == 0 {
+	// 		c := color.New(color.FgGreen)
+	// 		c.Println("Use traceip trace [...ip] to geo locate a collection of IPs or visit https://github.com/akashchouhan16/traceip for more details.")
+	// 	}
+	// },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -49,5 +54,6 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("ipv4", "4", false, "Enforce IPv4 geo tracing only {wip}")
+	rootCmd.Flags().BoolP("ipv6", "6", false, "Enforce IPv6 geo tracing only {wip}")
 }
--- cmd/trace.go
+++ cmd/trace.go
@@ -1,7 +1,7 @@
-/*
-Copyright © 2023 akashchouhan16/traceip <akash.c1500@gmail.com>
-
-*/
+/* ***********************************************
+ * copyright: 2023 @github.com/akashchouhan16/traceip
+ * author: @akashchouhan16
+ *************************************************/
 package cmd
 
 import (
@@ -17,26 +17,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type Ip struct {
-	IP       string `json:"ip"`
-	City     string `json:"city"`
-	Region   string `json:"region"`
-	Country  string `json:"country"`
-	Loc      string `json:"loc"`
-	Timezone string `json:"timezone"`
-	Postal   string `json:"postal"`
-}
-
 type flag struct {
 	forceV4 bool `default:false`
 	forceV6 bool `default:false`
 }
 
 var (
-	WarningLog *log.Logger
-	InfoLog    *log.Logger
-	ErrorLog   *log.Logger
-	CLIFlags   flag
+	Warn     *log.Logger
+	Info     *log.Logger
+	Error    *log.Logger
+	CLIFlags flag
 )
 
 type GeoLocation struct {
@@ -54,29 +44,29 @@ type GeoLocation struct {
 // traceCmd represents the trace command
 var traceCmd = &cobra.Command{
 	Use:   "trace",
-	Short: color.HiGreenString("Geo locate an IP v4 or IP v6 with the trace command."),
-	Long:  color.HiGreenString(`Usage: traceip trace <IP v4/ IP v6> command allows to geo locate the ip address.`),
+	Short: "Geo locate an IPv4 or IPv6 with the trace command.",
+	Long:  `Usage: traceip trace <ip address(es)> command allows to geo locate the ip address(es).`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) > 0 {
 			for _, ip := range args {
 				displayGeoData(ip)
 			}
 		} else {
-			WarningLog.Println(color.HiYellowString("Please provide an IP v4 or IP v6 with trace command to Geo Locate the IP address."))
+			Warn.Println(color.HiYellowString("Please provide an IP v4 or IP v6 with trace command to Geo Locate the IP address."))
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(traceCmd)
-	InfoLog = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	WarningLog = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-	ErrorLog = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(os.Stdout, "[INFO]: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Warn = log.New(os.Stdout, "[WARNING]: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(os.Stdout, "[ERROR]: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func displayGeoData(ip string) {
 	if net.ParseIP(ip) == nil {
-		WarningLog.Println(color.HiYellowString("Error: Invalid IP address. Please try again with a valid ip."))
+		Error.Println(color.HiRedString("Invalid IP address. Please try again with a valid ip."))
 		// fmt.Println("Invalid IP v4 / IP v6 provided. Please try again with a valid IP address")
 		return
 	} else {
@@ -87,16 +77,16 @@ func displayGeoData(ip string) {
 
 		err := json.Unmarshal(responseByte, &data)
 		if err != nil {
-			ErrorLog.Println(color.HiRedString("Failed to unmarshal JSON response."))
+			Error.Println(color.HiRedString("Failed to unmarshal JSON response."))
 			// log.Println("Unable to unmarshal the response")
 		}
 
-		c := color.New(color.FgRed).Add(color.Underline).Add(color.Bold)
-		c.Println("DATA :")
+		c := color.New(color.FgGreen).Add(color.Underline).Add(color.Bold)
+		c.Println("==============================")
 
-		c = color.New(color.FgHiYellow).Add(color.Bold)
-		c.Printf("IP: %s\nCITY: %s\nREGION: %s\nCOUNTRY :%s\nLOCATION :%s\nTIMEZONE:%s\nPOSTAL: %s\nOrg: %s\nDoc: %s\n", data.IP, data.City, data.Region, data.Country, data.Loc, data.Timezone, data.Postal, data.Org, data.Readme)
-
+		c = color.New(color.FgHiGreen).Add(color.Bold)
+		c.Printf("	- IP: %s\n	- CITY: %s\n	- REGION: %s\n	- COUNTRY :%s\n	- LOCATION :%s\n	- TIMEZONE:%s\n	- POSTAL: %s\n	- Org: %s\n", data.IP, data.City, data.Region, data.Country, data.Loc, data.Timezone, data.Postal, data.Org)
+		c.Println("==============================")
 	}
 
 }
@@ -111,12 +101,12 @@ func traceIP(url string) []byte {
 	response, err := http.Get(url)
 
 	if err != nil {
-		ErrorLog.Println(color.HiRedString("Network call for IP geo locate failed: %s", err))
+		Error.Println(color.HiRedString("Network call for IP geo locate failed: %s", err))
 	}
 
 	responseInBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		ErrorLog.Println(color.HiRedString("IO Util read failed to generate byte response: %s", err))
+		Error.Println(color.HiRedString("IO Util read failed to generate byte response: %s", err))
 	}
 
 	return responseInBytes
